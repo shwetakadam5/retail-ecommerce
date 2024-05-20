3f9d74afa242d6723069aaf4fc07b1c69483cf28
shwetakadam5 <159270090+shwetakadam5@users.noreply.github.com>
2024-05-20 16:07:09 +1000

    Updated the routes for tag : get, get by id, delete by id, post and update by id

--- db/queries.sql
+++ db/queries.sql
@@ -9,3 +9,9 @@ FROM product p
 JOIN category c ON c.id = p.category_id
 JOIN product_tag pt ON pt.product_id = p.id
 JOIN tag t ON t.id = pt.tag_id
+
+-- Query to fetch all tags along with product details
+SELECT pt.id, t.id as tagId, t.tag_name ,pt.product_id, p.product_name
+FROM tag t
+JOIN product_tag pt ON pt.tag_id = t.id
+JOIN product p ON p.id = pt.product_id
